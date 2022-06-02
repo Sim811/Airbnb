d9dff62eae158ec45ec80a1013026a63d70d8a77
Sim <skmatagi@hotmail.com>
2022-06-02 16:10:05 -0600

    search component complete

--- components/Header.js
+++ components/Header.js
@@ -10,13 +10,15 @@ import {useState} from 'react';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
+import { useRouter } from "next/router";
 
 
-function Header() {
+function Header({placeholder}) {
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
 
   const selectionRange = {
     startDate: startDate,
@@ -33,11 +35,25 @@ function Header() {
     setSearchInput('');
   }
 
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+      }
+    });
+  }
+
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left of Header */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div 
+        onClick={() => router.push("/")} 
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image 
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -56,7 +72,7 @@ function Header() {
         text-gray-600 placeholder-gray-400
         flex-grow" 
         type="text" 
-        placeholder="Start your search" />
+        placeholder={ placeholder || "Start your search"} />
         <SearchIcon className="hidden 
         md:inline-flex h-8 bg-red-400 
         text-white rounded-full p-2 cursor-pointer md:mx-2 " />
@@ -89,7 +105,7 @@ function Header() {
             <input 
               value={noOfGuests}
               onChange ={e => setNoOfGuests(e.target.value )}
-              min={1 }
+              min={1}
               type='number' 
               className='w-12 pl-2 text-lg outline-none text-red-400' 
             />
@@ -98,7 +114,9 @@ function Header() {
             <button onClick={resetInput} className='flex-grow text-gray-500'>
               Cancel
             </button>
-            <button className='flex-grow text-red-400'>
+            <button
+            onClick={search}
+            className='flex-grow text-red-400'>
               Search
             </button>
           </div>
